f7b8a68852acd9a08b5ded4eabe4b24ce2472001
bonnw <pwonghansa@gmail.com>
2019-02-14 17:21:11 -0600

    progress made on findOneByGDP route

--- src/main/java/com/burahan/gdpdemo/CountryController.java
+++ src/main/java/com/burahan/gdpdemo/CountryController.java
@@ -2,10 +2,7 @@ package com.burahan.gdpdemo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -49,6 +46,33 @@ public class CountryController
     public List<Country> allGDPSorted()
     {
         return cRepo.findAll().stream().sorted(Comparator.comparing(Country::getGdp)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/countries/total")
+    public Long returnGDPTotal()
+    {
+        List<Country> cList = cRepo.findAll();
+        Long runningTotal = 0L;
+        for (Country c : cList)
+        {
+            runningTotal += c.getGdp();
+        }
+        return runningTotal;
+    }
+
+    @GetMapping("/gdp/{country name}")
+    public Country findOneByGDP(@PathVariable String country)
+    {
+        List<Country> cList = cRepo.findAll();
+        for (Country c : cList)
+        {
+            if (c.getCountry().equals(country))
+            {
+                return c;
+            }
+        }
+
+        return null;
     }
 
 }
