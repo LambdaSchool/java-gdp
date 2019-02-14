6e3a4b75b80c8817e9381998b3355b15232dddbe
bonnw <pwonghansa@gmail.com>
2019-02-14 13:49:00 -0600

    initialized project with intelliJ

---
+++ src/main/java/com/burahan/gdpdemo/GdpDemoApplication.java
@@ -0,0 +1,16 @@
+package com.burahan.gdpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdpDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GdpDemoApplication.class, args);
+    }
+
+}
+
