5d2913062e601fc3ce070d2f6226b2d8994ab437
JasonCorp84 <csababalogh84@gmail.com>
2019-02-14 13:38:41 -0800

    Project Initiated

---
+++ src/main/java/com/lambdaschool/javagdp/JavagdpApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.javagdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavagdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavagdpApplication.class, args);
+    }
+
+}
+
